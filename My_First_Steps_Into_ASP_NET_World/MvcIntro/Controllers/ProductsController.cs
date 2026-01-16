3861c17d0d43db52bff9f97f10e4cf7990924064
Alex <mladtigyr@gmail.com>
2026-01-16 15:17:26 +0200

    First look of ASP.Net

---
+++ My_First_Steps_Into_ASP_NET_World/MvcIntro/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcIntro.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.PageTitle = "Our Products";
+            return View();
+        }
+
+        [Route("Products/{id}")]
+        [Route("Products/Details/{id}")]
+        public IActionResult Details(int id)
+        {
+            if (id < 1)
+            {
+                return Ok("Invalid product id");
+            }
+
+            return Ok($"Product Details for Product ID: {id}");
+        }
+    }
+}
